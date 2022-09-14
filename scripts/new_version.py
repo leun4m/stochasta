2c8a38d7e919e80bbe2d3d307491134e0368a7eb
Manuel Krebs <me@leun4m.de>
2022-09-14 22:21:27 +0200

    Add script to publish new releases

---
+++ scripts/new_version.py
@@ -0,0 +1,100 @@
+#!/usr/bin python3
+
+# Script to create a new version.
+# The env must provide a GITHUB_API_TOKEN.
+#
+# - sets version in Cargo.toml
+# - sets version in CHANGELOG.md
+# - commits those files
+# - creates annoted version tag
+# - publishes the new release on GitHub
+#   (with the RELEASE NOTES from CHANGELOG)
+
+
+import os
+import datetime
+import requests
+from dotenv import load_dotenv
+
+
+def main():
+    version = input("Version: ")
+    date = datetime.datetime.now().date().isoformat()
+
+    release_notes = get_release_notes()
+    update_cargo(version)
+    update_changelog(version, date)
+    os.system("git diff")
+
+    accept = input("Commit? [y/N]")
+    if accept.lower() == "y":
+        os.system(f"git add Cargo.toml CHANGELOG.md")
+        os.system(f"git commit -m 'Update version'")
+        os.system(f"git tag -a v{version} -m ''")
+        os.system(f"git push --follow-tags")
+
+    accept = input("Publish? [y/N]")
+    if accept.lower() == "y":
+        publish_release(f"v{version}", release_notes)
+        os.system(f"rm RELEASE_NOTES")
+
+
+def get_release_notes():
+    with open("CHANGELOG.md", "r", encoding="utf-8") as file:
+        data = file.read()
+        notes = data.split("\n## Unreleased\n")[1].split("\n## ")[0].strip()
+        notes += "\n\n---\n\n"
+        notes += "*For a complete history see: [CHANGELOG.md](./CHANGELOG.md)*"
+
+    with open("RELEASE_NOTES", "w", encoding="utf-8") as file:
+        file.write(notes)
+
+    return notes
+
+
+def update_cargo(version):
+    with open("Cargo.toml", "r", encoding="utf-8") as file:
+        data = file.readlines()
+        if data[2].startswith("version"):
+            data[2] = f"version = \"{version}\"\n"
+
+    with open("Cargo.toml", "w", encoding="utf-8") as file:
+        file.writelines(data)
+
+
+def update_changelog(version, date):
+    with open("CHANGELOG.md", "r", encoding="utf-8") as file:
+        data = file.read()
+        data = data.replace("\n## Unreleased\n",
+                            f"\n## [{version}] ({date})\n")
+        data += f"[{version}]: https://github.com/leun4m/stochasta/releases/tag/v{version}\n"
+
+    with open("CHANGELOG.md", "w", encoding="utf-8") as file:
+        file.write(data)
+
+
+def publish_release(version, release_notes):
+    owner = "leun4m"
+    repo = "stochasta"
+    url = f"https://api.github.com/repos/{owner}/{repo}/releases"
+    obj = {
+        "owner": owner,
+        "repo": repo,
+        "tag_name": version,
+        #   "target_commitish": 'develop',
+        "name": version,
+        "body": release_notes,
+        "draft": False,
+        "prerelease": False,
+        "generate_release_notes": False
+    }
+    token = os.getenv("GITHUB_API_TOKEN")
+    requests.post(url, json=obj, headers={
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}"
+    })
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
