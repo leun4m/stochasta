9721227b9365de79479c02df78ddc83aa10f256d
Manuel Krebs <me@leun4m.de>
2022-09-20 00:16:42 +0200

    Clippy

--- examples/dice_same_number.rs
+++ examples/dice_same_number.rs
@@ -12,15 +12,7 @@ fn main() {
 
     let dice = CardDeck::from(vec![1, 2, 3, 4, 5, 6]);
     let tree = CardDrawTree::without_shrinking(&dice, throws);
-    let probabiltiy = tree.probability_of(&repeats(throws));
+    let probabiltiy = tree.probability_of(&vec![6; throws as usize]);
 
     println!("Probability of throwing {}x a six: {}", throws, probabiltiy);
-}
-
-fn repeats(draws: u32) -> Vec<u8> {
-    let mut result = Vec::new();
-    for _ in 0..draws {
-        result.push(6);
-    }
-    result
 }
